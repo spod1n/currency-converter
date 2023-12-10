1f89bbe1f0cd6ccf7c66bba070bddf1751e27e3a
spod1n <k.spodin@gmail.com>
2023-12-10 23:47:58 +0200

    delete button markup. add line key board button.

--- process/markup.py
+++ process/markup.py
@@ -1,9 +1,21 @@
 """ Buttons and other settings """
 
+import pandas as pd
 from telebot import types
 
-def markup():
-    markup = types.ReplyKeyboardMarkup(one_time_keyboard=True)
-    markup.add('Add new button ', 'Add second button  2')
 
-    return markup
+def get_text_buttons(rate_data: pd.DataFrame, param_name: str, filter_value=None):
+    keyboard = types.InlineKeyboardMarkup(row_width=2)
+
+    if param_name == 'CodeNameA':
+        options = rate_data['CodeNameA'].unique().tolist()
+    elif param_name == 'CodeNameB':
+        filter_rate_data = rate_data[rate_data['CodeNameA'] == filter_value]
+        options = filter_rate_data['CodeNameB'].unique().tolist()
+    else:
+        options = []
+
+    for option in options:
+        button = types.InlineKeyboardButton(text=option, callback_data=option)
+        keyboard.add(button)
+    return keyboard
